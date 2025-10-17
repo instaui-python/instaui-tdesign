8080563305bbccb7cc54b19ba4b30efd70ce06ca
CrystalWindSnake <568166495@qq.com>
2025-10-18 01:31:38 +0800

    feat: locales.use_locale_dict

--- src/instaui_tdesign/locales/_index.py
+++ src/instaui_tdesign/locales/_index.py
@@ -1,5 +1,6 @@
 import importlib
-from typing import Dict
+from typing import Literal, Optional
+from instaui import ui
 from instaui_tdesign.types import TLocale
 
 
@@ -27,5 +28,61 @@ class LazyLocaleDict:
 _locale_dict = LazyLocaleDict()
 
 
-def get_locale(locale: TLocale) -> Dict:
+def get_locale(locale: TLocale) -> dict:
+    """
+    Get locale dict by locale name.
+    Args:
+        locale (TLocale): locale name, e.g. "zh-CN", "en-US"
+    """
     return _locale_dict.get(locale)
+
+
+def get_locales(locales: list[TLocale]) -> dict:
+    """
+    Get locale dicts by locale names.
+    Args:
+        locales (list[TLocale]): locale names, e.g. ["zh-CN", "en-US"]
+    """
+    return {locale: get_locale(locale) for locale in locales}
+
+
+def use_locale_dict(
+    *,
+    pre_load_locales: Optional[list[TLocale]] = None,
+    type: Literal["server", "client"] = "server",
+) -> tuple[dict, str]:
+    """
+    Get locale dict and language by locale names.
+
+    Args:
+        pre_load_locales (Optional[list[TLocale]]): locale names to pre-load, e.g. ["zh-CN", "en-US"]. Defaults to ["en-US", "zh-CN"].
+        type (Literal[&quot;server&quot;, &quot;client&quot;], optional): The type of locale dict, "server" or "client". Defaults to "server".
+
+    Examples:
+    .. code-block:: python
+        locale_dict, lang = use_locale_dict(pre_load_locales=["en-US", "zh-CN"])
+
+        td.select(["en-US", "zh-CN"], lang)
+
+        with td.config_provider(global_config=locale_dict):
+            td.input()
+    """
+
+    pre_load_locales = pre_load_locales or ["en-US", "zh-CN"]
+
+    lang = ui.use_language()
+
+    if type == "server":
+
+        @ui.computed(inputs=[get_locales(pre_load_locales), lang])
+        def locale_dict(locales: dict, lang: str) -> dict:
+            return locales[lang.replace("_", "-")]
+
+        return locale_dict, lang
+
+    locale_dict = ui.js_computed(
+        inputs=[get_locales(pre_load_locales), lang],
+        code=r"(locales,lang) =>  locales[lang.replace('_','-')]",
+    )
+
+    return locale_dict, lang
--- src/instaui_tdesign/types.py
+++ src/instaui_tdesign/types.py
@@ -4,21 +4,6 @@ from typing import Any, Dict, Literal
 TLocale = Literal[
     "zh-CN",
     "en-US",
-    "ja-JP",
-    "zh-TW",
-    "pt-PT",
-    "es-ES",
-    "id-ID",
-    "fr-FR",
-    "de-DE",
-    "ko-KR",
-    "it-IT",
-    "ms-MY",
-    "th-TH",
-    "vi-VN",
-    "km-KH",
-    "ar-EG",
-    "ru-RU",
 ]
 
 TCustomizeLocale = Dict[str, Any]
