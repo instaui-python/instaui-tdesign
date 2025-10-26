87c23a0ecb8cceb20d48d817f6b69b4eba772a87
CrystalWindSnake <568166495@qq.com>
2025-10-27 04:24:23 +0800

    feat:td.notify_plugin.info()

--- src/instaui_tdesign/components/notification.py
+++ src/instaui_tdesign/components/notification.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 import typing
-
+from typing_extensions import TypedDict, Unpack
+import copy
+from instaui import ui
 from instaui_tdesign.components._icon_param_utils import make_icon_for_bool_or_str
 from ._base_element import BaseElement
 from instaui.event.event_mixin import EventMixin
-from typing_extensions import TypedDict, Unpack
 from ._utils import handle_props, handle_event_from_props
 
 if typing.TYPE_CHECKING:
@@ -14,9 +15,9 @@ if typing.TYPE_CHECKING:
 class Notification(BaseElement):
     def __init__(
         self,
-        content: typing.Optional[TMaybeRef[str]],
+        content: typing.Optional[str] = None,
         *,
-        title: typing.Optional[TMaybeRef[str]] = None,
+        title: typing.Optional[str] = None,
         icon: typing.Union[str, bool, None] = None,
         **kwargs: Unpack[TNotificationProps],
     ):
@@ -33,7 +34,7 @@ class Notification(BaseElement):
         extends: typing.Optional[typing.List] = None,
     ):
         self.on(
-            "close_btn_click",
+            "close-btn-click",
             handler,
             extends=extends,
         )
@@ -46,17 +47,47 @@ class Notification(BaseElement):
         extends: typing.Optional[typing.List] = None,
     ):
         self.on(
-            "duration_end",
+            "duration-end",
             handler,
             extends=extends,
         )
         return self
 
 
+class NotifyPlugin:
+    @staticmethod
+    def info(**options: Unpack[TNotificationOptions]):
+        return ui.js_event(
+            inputs=[_handle_notification_options(typing.cast(dict, options))],
+            code=r"(options)=> $tdesign.NotifyPlugin.info(options)",
+        )
+
+
 class TNotificationProps(TypedDict, total=False):
-    close_btn: TMaybeRef[typing.Union[str, bool]]
-    duration: TMaybeRef[float]
+    close_btn: typing.Union[str, bool]
+    duration: float
     footer: TMaybeRef[str]
     theme: TMaybeRef[typing.Literal["info", "success", "warning", "error"]]
     on_close_btn_click: EventMixin
     on_duration_end: EventMixin
+
+
+class TNotificationOptions(TypedDict, total=False):
+    content: str
+    title: str
+    attach: str
+    offset: tuple[typing.Union[str, int], typing.Union[str, int]]
+    placement: typing.Literal["top-right", "top-left", "bottom-right", "bottom-left"]
+    zIndex: int
+
+    close_btn: typing.Union[str, bool]
+    duration: float
+    footer: TMaybeRef[str]
+
+
+def _handle_notification_options(options: dict):
+    options = copy.deepcopy(options)
+    if "close_btn" in options:
+        options["closeBtn"] = options.pop("close_btn")
+
+    return options
