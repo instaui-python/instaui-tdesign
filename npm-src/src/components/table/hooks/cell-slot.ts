c0d736997835dcb9aec3f5ca52262aa752f555be
CrystalWindSnake <568166495@qq.com>
2025-11-05 02:07:34 +0800

    feat: enhance td.table

---
+++ npm-src/src/components/table/hooks/cell-slot.ts
@@ -0,0 +1,13 @@
+import type { TTableColumnsWithInfer } from "../types";
+
+export function withCellSlotPropConverter(columns: TTableColumnsWithInfer) {
+  const cellSlotNames = new Set(columns.value.map((col) => col.cell as string));
+
+  return (slotName: string | number, orgProps: any) => {
+    const currentValue = orgProps.row[orgProps.col.colKey];
+
+    return cellSlotNames.has(slotName as string)
+      ? { ...orgProps, currentValue }
+      : orgProps;
+  };
+}
